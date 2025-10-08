269403081fd42b35c8f523a6baea608b5701cdc9
Thomas Ngo <s4006361@student.rmit.edu.au>
2025-10-08 15:49:13 +1100

    new enemy and starting code logic for UI

---
+++ Assets/Scripts/UI/QuizQuestionUIInteraction.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class QuizQuestionUIInteraction : MonoBehaviour
+{
+    [SerializeField] UIDocument uIDocument;
+    private VisualElement root;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        root = uIDocument.rootVisualElement;
+        root.style.display = DisplayStyle.None; // Hide UI on startup
+    }
+
+    private void OnClickEvent(ClickEvent evt)
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
